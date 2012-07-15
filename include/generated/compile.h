e15e4077b4b1ea7e2e99e241b4c2e163cf876737
aftersilence <913704@mail.ru>
2012-07-15 17:11:45 +0400

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Jul 15 17:04:32 MSK 2012"
+#define LINUX_COMPILE_BY ""
+#define LINUX_COMPILE_HOST ""
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
